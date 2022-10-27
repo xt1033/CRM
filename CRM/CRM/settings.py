275e161fc9fd6b9eb3c093abb648c7ae199dcd51
xt1033 <abhi.negi9@gmail.com>
2022-10-27 19:24:16 +0530

    Added procfile and modfied staticfiles link

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wkxbu@f#35%nw3sxpu0^4x078%=p#bg(80y3kzm9mr73o_*xzn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['customer-relationship-manager1.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,6 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
 
