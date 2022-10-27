4d047fdbf85d62777fc2393b39b7b6c9adbaf838
xt1033 <abhi.negi9@gmail.com>
2022-10-27 16:43:37 +0530

    Added the initial app

---
+++ CRM/accounts/decorators.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+# decorator is a function that takes in another function as paramater and lets us add some extra functionality
+# before original function is called
+
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_func
+
+
+def allowed_users(allowed_rules=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+            if group in allowed_rules:
+                print('Working : ', allowed_rules)
+            else:
+                return HttpResponse('You are not authorised to view this page')
+            return view_func(request, *args, **kwargs)
+        return wrapper_func
+    return decorator
+
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'customer':
+            return redirect('user-page')
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
---
+++ CRM/accounts/urls.py
@@ -0,0 +1,41 @@
+"""Accounts URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name="home"),
+    path('user/', views.userPage, name="user-page"),
+    path('account/', views.accountSettings, name="account"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_done.html"), name="password_reset_complete"),
+]
